6293730d7da976868565379bd8e07ab5c81559a0
Alexis Brissard <alexis.brissard@gmail.com>
2016-03-27 19:43:41 +0200

    Complete id obfuscation

--- server/app/index.js
+++ server/app/index.js
@@ -33,6 +33,8 @@ function format_game(game) {
   return JSON.parse(JSON.stringify(game, function(key, value) {
     if (key === "socket") {
       return undefined;
+    } else if (key === "id") {
+      return encryption.encrypt(value);
     } else {
       return value;
     }
@@ -75,7 +77,7 @@ io.on('connection', function(socket){
     games[game.id] = game;
 
     if (fn) {
-      fn({ result: 'ok', id: game.id });
+      fn({ result: 'ok', id: encryption.encrypt(game.id) });
     }
 
     console.log("Creating game");
@@ -90,6 +92,7 @@ io.on('connection', function(socket){
 
   socket.on('join_game', function(data, fn) {
     const id = encryption.decrypt(data.code);
+    console.log(id);
     let game = games[id];
     console.log("Join game id:" + game.id);
     if (game.state.name === states.WAITING_PLAYERS.name) {
@@ -113,7 +116,7 @@ io.on('connection', function(socket){
       //TODO Handle reconnection
     }
     if (fn) {
-      fn({ result: 'ok', id: game.id });
+      fn({ result: 'ok', id: encryption.encrypt(game.id) });
     }
     updateAllPlayers(game);
   });
