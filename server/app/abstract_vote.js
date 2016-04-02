b5d7a64fbb8d52b4d51a23912e095545dea33212
thomas.wilgenbus <thomas.wilgenbus@happn.com>
2016-04-02 23:44:13 +0200

    Add abstract vote result

--- server/app/abstract_vote.js
+++ server/app/abstract_vote.js
@@ -1,6 +1,6 @@
 
 module.exports.vote = function (game, player, param) {
-  if (game.state.name === state.name && !(player.vote)) {
+  if (!(player.vote)) {
     var candidate = game.players.findByPseudo(param.player_pseudo);
     if (candidate) {
       if (candidate.vote_count) {
@@ -14,3 +14,30 @@ module.exports.vote = function (game, player, param) {
     game.updateAllPlayers();
   }
 };
+
+module.exports.validate_vote = function (game, onVoteSuccess, onVoteFail) {
+  var winners = game.players.reduce(function (prev, cur) {
+    if (!Array.isArray(prev)) {
+      prev = [prev];
+    }
+    var prev_count = prev[0].vote_count || 0;
+    var cur_count = cur.vote_count || 0;
+    if (prev_count < cur_count) {
+      return [cur];
+    } else if (prev_count > cur_count){
+      return prev;
+    } else {
+      prev.push(cur);
+      return prev;
+    }
+  });
+  game.players.forEach(function (player) {
+    delete player.vote;
+    delete player.vote_count;
+  })
+  if(winners.length > 1) {
+    console.log("winners: " + winners.map(w => w.pseudo));
+    return onVoteSuccess(game, winners[0]);
+  }
+  return onVoteFail(game);
+}
--- server/app/states/day_result.js
+++ server/app/states/day_result.js
@@ -1,47 +1,26 @@
+const abstract_vote = require("../abstract_vote.js");
+
 let state = {
   name: "DAY_RESULT",
   next: function(game) {
     const states = require("../states.js");
-    var winners = game.players.reduce(function (prev, cur){
-      if (!Array.isArray(prev)) {
-        prev = [prev];
-      }
-      var prev_count = prev[0].vote_count || 0;
-      var cur_count = cur.vote_count || 0;
-      if (prev_count < cur_count) {
-        return [cur];
-      } else if (prev_count > cur_count){
-        return prev;
-      } else {
-        prev.push(cur);
-        return prev;
-      }
-    });
-    console.log("winners: " + winners.map(w => w.pseudo));
-    var next_state;
-    if(winners.length > 1) {
-      next_state = states.DAY_VOTE;
-    } else {
-      if (game.turn == 0) {
-        winners[0].captain = true;
-        console.log(winners[0].pseudo + " was elected as the new leader !");
-        next_state = states.NIGHT;
-      } else {
-        winners[0].dead = "DAY_VOTE";
-        console.log(winners[0].pseudo + " was designated as the next hanged !");
-        if (winners[0].role.name === "HUNTER") {
-          game.after_hunter_revenge = states.NIGHT;
-          next_state = states.HUNTER_REVENGE;
-        } else {
-          next_state = states.NIGHT;
+    return abstract_vote.validate_vote(game,
+      (game) => states.DAY_VOTE,
+      (game, result) => {
+        if (game.turn == 0) {
+          winner.captain = true;
+          console.log(winner.pseudo + " was elected as the new leader !");
+          return states.NIGHT;
         }
+        winner.dead = "DAY_VOTE";
+        console.log(winner.pseudo + " was designated as the next hanged !");
+        if (winner.role.name === "HUNTER") {
+          game.after_hunter_revenge = states.NIGHT;
+          return states.HUNTER_REVENGE;
+        }
+        return states.NIGHT;
       }
-    }
-    game.players.forEach(function (player) {
-      delete player.vote;
-      delete player.vote_count;
-    })
-    return next_state;
+    );
   },
   actions: []
 }
