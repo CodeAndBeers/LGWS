3b66bdc8422a81674b706d5c5ba0fd4fe03a2794
Alexis Brissard <alexis.brissard@gmail.com>
2016-04-10 23:15:24 +0200

    Add state morning

--- server/app/states/day_vote.js
+++ server/app/states/day_vote.js
@@ -5,14 +5,6 @@ const abstract_vote = require("../abstract_vote.js")
 let state = {
   name: "DAY_VOTE",
   init: function(game) {
-    var last_dead = game.players.find(p => p.last_dead);
-    if (last_dead) delete last_dead.last_dead;
-    game.turn++;
-    let hunter = game.players.getHunter();
-    if (hunter && hunter.dead && hunter.dead !== "NONE" && !hunter.take_revenge) {
-      game.after_hunter_revenge = state;
-      return HUNTER_REVENGE;
-    }
   },
   next: function(game) {
     return DAY_RESULT;
---
+++ server/app/states/morning.js
@@ -0,0 +1,23 @@
+let DAY_VOTE = require("./day_vote.js");
+
+let state = {
+  name: "MORNING",
+  init: function(game) {
+    var last_dead = game.players.find(p => p.last_dead);
+    if (last_dead) delete last_dead.last_dead;
+    game.turn++;
+    let hunter = game.players.getHunter();
+    if (hunter && hunter.dead && hunter.dead !== "NONE" && !hunter.take_revenge) {
+      game.after_hunter_revenge = next();
+      return HUNTER_REVENGE;
+    }
+    //Nothing to do in morning
+    return this.next();
+  },
+  next: function(game) {
+      return DAY_VOTE;
+  },
+  actions: []
+}
+
+module.exports = state;
