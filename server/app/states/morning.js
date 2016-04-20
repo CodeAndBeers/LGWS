28579d505b4ac74d8399adfc8a20608be12be68b
thomas.wilgenbus <thomas.wilgenbus@happn.com>
2016-04-20 19:40:12 +0200

    Fix last_dead reset for ALL players instead of 1st

--- server/app/states/morning.js
+++ server/app/states/morning.js
@@ -4,8 +4,9 @@ const HUNTER_REVENGE = require("./hunter_revenge.js");
 let state = {
   name: "MORNING",
   init: function(game) {
-    var last_dead = game.players.find(p => p.last_dead);
-    if (last_dead) delete last_dead.last_dead;
+    // reset all players last_dead variable
+    for (let player in game.players) if (player.last_dead) delete player.last_dead;
+
     game.turn++;
     let hunter = game.players.getHunter();
     if (hunter && hunter.dead && hunter.dead !== "NONE" && !hunter.take_revenge) {
